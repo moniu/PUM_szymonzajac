f1636683b1637756a9289e4f265f9715dfa76d20
SzymonZajac <moniukins@gmail.com>
2022-10-06 17:30:06 +0200

    Canvas now changes color between green and blue

--- lab1/script.js
+++ lab1/script.js
@@ -1,12 +1,21 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+let time = 0;
+
 var refreshInterval = setInterval(() => {
-    let canvas = document.getElementById("canvas1");
-    let ctx = canvas.getContext("2d");
     ctx.fillStyle = "#333333";
     ctx.fillRect(0,0,800,600);
 
-
-    ctx.fillStyle = "#00FF00";
+    if (time%2 == 0) {
+        ctx.fillStyle = "#00FF00";
+    }
+    else {
+        ctx.fillStyle = "#0000FF";
+    }
+    
     ctx.arc(400, 300, 100, 0, Math.PI*2);
     ctx.fill();
+
+    time++;
 
 }, 1000)
