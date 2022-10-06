72ff9f39f417439997c03c8214260c18900a48fc
SzymonZajac <moniukins@gmail.com>
2022-10-06 17:26:01 +0200

    Added drawing of a green circle

--- lab1/script.js
+++ lab1/script.js
@@ -1,4 +1,12 @@
 var refreshInterval = setInterval(() => {
     let canvas = document.getElementById("canvas1");
     let ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#333333";
+    ctx.fillRect(0,0,800,600);
+
+
+    ctx.fillStyle = "#00FF00";
+    ctx.arc(400, 300, 100, 0, Math.PI*2);
+    ctx.fill();
+
 }, 1000)
