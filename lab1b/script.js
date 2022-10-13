b0bf7bc2256dea493e316b472b32be2a969e8db9
SzymonZajac <moniukins@gmail.com>
2022-10-13 16:45:41 +0200

    Ball moves up and down correctly

--- lab1b/script.js
+++ lab1b/script.js
@@ -3,14 +3,16 @@ let ctx = canvas.getContext("2d");
 let time = 0;
 
 var refreshInterval = setInterval(() => {
+    
     ctx.fillStyle = "#333333";
     ctx.fillRect(0,0,800,600);
 
     let color = Math.abs(time%510-255);
     ctx.fillStyle = "rgb(0,"+color+","+(255-color)+")";
 
-    let position = 450 + 150*Math.sin(time/(24*Math.PI));
+    let position = 350 + 150*Math.sin(time/(12*Math.PI));
 
+    ctx.beginPath()
     ctx.arc(400, position, 100, 0, Math.PI*2);
     ctx.fill();
 
