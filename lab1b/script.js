e7a35ad152c1d0b8ca5ac75f2e1f7805a31a287d
SzymonZajac <moniukins@gmail.com>
2022-10-13 16:38:39 +0200

    Removed unnessesary logging

--- lab1b/script.js
+++ lab1b/script.js
@@ -7,10 +7,8 @@ var refreshInterval = setInterval(() => {
     ctx.fillRect(0,0,800,600);
 
     let color = Math.abs(time%510-255);
-    console.log(color)
     ctx.fillStyle = "rgb(0,"+color+","+(255-color)+")";
-    console.log(ctx.fillStyle);
-    
+
     ctx.arc(400, 300, 100, 0, Math.PI*2);
     ctx.fill();
 
