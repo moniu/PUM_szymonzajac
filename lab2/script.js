958c4f91ab15a82a6100798d5e8fb094eb313bf6
Szymon Zajac <moniukins@gmail.com>
2022-10-20 16:30:19 +0200

    Init repo, added floor and background

---
+++ lab2/script.js
@@ -0,0 +1,24 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+let time = 0;
+
+var refreshInterval = setInterval(() => {
+    
+    ctx.fillStyle = "#3784E6";
+    ctx.fillRect(0,0,800,600);
+
+    ctx.fillStyle = "#2D831B";
+    ctx.fillRect(0,400,800,200);
+
+    // let color = Math.abs(time%510-255);
+    // ctx.fillStyle = "rgb(0,"+color+","+(255-color)+")";
+
+    // let position = 350 + 150*Math.sin(time/(2*Math.PI));
+
+    // ctx.beginPath()
+    // ctx.arc(400, position, 100, 0, Math.PI*2);
+    // ctx.fill();
+
+    time++;
+
+}, 20)
