984b0a95e8cc9ac4580efa7c84ec9bafcbbaf5ea
Szymon Zajac <moniukins@gmail.com>
2022-11-03 16:47:44 +0100

    Added stripes on both sides

--- lab4/script.js
+++ lab4/script.js
@@ -17,6 +17,7 @@ ctx.font = '256px Consolas';
 
 var refreshInterval = setInterval(() => {
 
+    //bg
     ctx.fillStyle = "#3d893b";
     ctx.fillRect(000,0,800,600);
     ctx.fillStyle = "#808080";
@@ -24,20 +25,27 @@ var refreshInterval = setInterval(() => {
     ctx.fillStyle = "#994336";
     ctx.fillRect(100,0,10,600);
     ctx.fillRect(700,0,10,600);
-    
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(250, (5*time+100)%600, 10, 30);
-    ctx.fillRect(250, (5*time+400)%600, 10, 30);
-    ctx.fillRect(550, (5*time+100)%600, 10, 30);
-    ctx.fillRect(550, (5*time+400)%600, 10, 30);
 
+    // middle road stripes
+    ctx.fillStyle = "#FFF";
+    ctx.fillRect(250, (5*time+100)%600, 10, 100);
+    ctx.fillRect(250, (5*time+400)%600, 10, 100);
+    ctx.fillRect(550, (5*time+100)%600, 10, 100);
+    ctx.fillRect(550, (5*time+400)%600, 10, 100);
+
+    // side stripes
     ctx.fillRect(100, (5*time+100)%600, 10, 50);
     ctx.fillRect(100, (5*time+200)%600, 10, 50);
     ctx.fillRect(100, (5*time+300)%600, 10, 50);
     ctx.fillRect(100, (5*time+400)%600, 10, 50);
     ctx.fillRect(100, (5*time+500)%600, 10, 50);
     ctx.fillRect(100, (5*time+600)%600, 10, 50);
-    
+    ctx.fillRect(700, (5*time+100)%600, 10, 50);
+    ctx.fillRect(700, (5*time+200)%600, 10, 50);
+    ctx.fillRect(700, (5*time+300)%600, 10, 50);
+    ctx.fillRect(700, (5*time+400)%600, 10, 50);
+    ctx.fillRect(700, (5*time+500)%600, 10, 50);
+    ctx.fillRect(700, (5*time+600)%600, 10, 50);
 
     ctx.fillStyle = "#921A1A";
     ctx.fillRect(playerPosX - 25, playerPosY - 50, 50, 100);
